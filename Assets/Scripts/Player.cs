3e8de68091c3e4a279eb12b21f58709d06ba4e77
pen_is_wise <efimovrusl@gmail.com>
2022-08-15 01:21:02 +0100

    post-commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,23 +6,23 @@ public class Player : MonoBehaviour
 {
     public event UnityAction OnDeath;
     public event UnityAction OnTouchFinishPlatform;
-    private Rigidbody _rigidbody;
+    private Rigidbody rigbody;
 
     private int floorsPassedWithoutTouch = 0;
     private bool CanBreakAnyPlatform => floorsPassedWithoutTouch > 2;
 
     private void Awake()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        rigbody = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
         // doubling default gravity (default = 7.0 m/s^2)
-        _rigidbody.AddForce( Physics.gravity * Time.fixedDeltaTime, ForceMode.VelocityChange );
+        rigbody.AddForce( Physics.gravity * Time.fixedDeltaTime, ForceMode.VelocityChange );
 
         // limiting speed of free-fall for player
-        _rigidbody.velocity = Vector3.ClampMagnitude( _rigidbody.velocity, 7 );
+        rigbody.velocity = Vector3.ClampMagnitude( rigbody.velocity, 7 );
     }
 
     public void TouchPlatform( FloorRoot floorRoot )
@@ -55,7 +55,7 @@ public class Player : MonoBehaviour
         else
         {
             OnDeath?.Invoke();
-            _rigidbody.velocity = Vector3.zero;
+            rigbody.velocity = Vector3.zero;
         }
     }
 
